c4bd6320105a550eff7afce2c626183f086279d3
mxlai <maxslie@gmail.com>
2015-02-16 18:33:09 -0500

    hadouken

--- js/app.js
+++ js/app.js
@@ -9,16 +9,28 @@ $(document).ready(function () {
 	})
 	.mousedown(function() {
 		console.log('mousedown');
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// hadouken sound
-		// show hadouken and animate right
+		$('.hadouken').finish().show().animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
 	})
 	.mouseup(function() {
 		console.log('mouseup');
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ready position
 	});
 });
+
+function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play(); 
+	}
+
